3330f8d5d58389960279a0045a4e2ebb9ada35db
k0pernicus <dev@carette.xyz>
2023-12-15 01:20:21 +0100

    Solved Day14 Part 2

--- Sources/Exercises/14.swift
+++ Sources/Exercises/14.swift
@@ -93,7 +93,7 @@ class Day14 : Day {
                 }
             }
         }
-        return currentLine
+        return String(currentLine.reversed())
     }
     
     internal func part02(fromContent: String) throws -> Output02 {
@@ -102,18 +102,15 @@ class Day14 : Day {
         var puzzle: [[Int]] = Array.init(repeating: [0], count: lines[0].count)
         var stableRocks : [[Int]] = Array.init(repeating: [], count: lines[0].count)
         
-        var previousPuzzle: [[Int]] = []
+        var cycleChecker: [[String]: Int] = [:]
+        var cycleCount  : [Int] = []
+        
         for i in 0...1000000000 {
+            
+            // NORTH
+            
             puzzle      = Array.init(repeating: [0], count: lines[0].count)
             stableRocks = Array.init(repeating: [], count: lines[0].count)
-            
-            print("\(i)th tour")
-            for line in lines {
-                print("\t\(line)")
-            }
-            print("")
-            
-            if (i % 3 == 0 && i != 0) { break }
             
             for (lineIndex, line) in lines.enumerated() {
                 for position in 0..<line.count {
@@ -129,36 +126,106 @@ class Day14 : Day {
                 }
             }
             
-            if (i % 4 == 0) { // North, West, South, East
-                if previousPuzzle == puzzle { break }
-                else { previousPuzzle = puzzle }
-            }
-            
             var newPuzzleLines: [String] = []
             for position in 0..<lines[0].count {
                 newPuzzleLines.append(getLineUsing(rocks: stableRocks[position], puzzleLine: puzzle[position], len: lines.count))
             }
             lines = newPuzzleLines
-        }
-        
-        var ans = 0
-        
-        let (height, width) = (puzzle.count, puzzle[0].count)
-        // Keep stableRocks as north's option
-        for j in 0..<height {
-            var sum = 0
-            for i in 0..<width {
-                if stableRocks[i].count > 0 && j == stableRocks[i].first! {
-                    let _ = stableRocks[i].removeFirst()
-                    if !puzzle[i].isEmpty { let _ = puzzle[i].removeFirst() }
-                    continue
+            
+            // WEST
+            
+            puzzle      = Array.init(repeating: [0], count: lines[0].count)
+            stableRocks = Array.init(repeating: [], count: lines[0].count)
+            
+            for (lineIndex, line) in lines.enumerated() {
+                for position in 0..<line.count {
+                    if line.characterAt(at: position) == "#" {
+                        stableRocks[position].append(lineIndex)
+                        puzzle[position].append(0)
+                    }
+                    else if line.characterAt(at: position) == "O" {
+                        var lastPuzzlePosition = puzzle[position].count - 1
+                        if lastPuzzlePosition == -1 { puzzle[position].append(0); lastPuzzlePosition += 1 }
+                        puzzle[position][lastPuzzlePosition] += 1
+                    }
                 }
-                if puzzle[i].isEmpty || puzzle[i].first! == 0 { continue }
-                puzzle[i][0] -= 1; sum += 1
             }
-            ans += sum * (height - j - 1)
+            
+            
+            newPuzzleLines = []
+            for position in 0..<lines[0].count {
+                newPuzzleLines.append(getLineUsing(rocks: stableRocks[position], puzzleLine: puzzle[position], len: lines.count))
+            }
+            lines = newPuzzleLines
+            
+            // SOUTH
+            
+            puzzle      = Array.init(repeating: [0], count: lines[0].count)
+            stableRocks = Array.init(repeating: [], count: lines[0].count)
+            
+            for (lineIndex, line) in lines.enumerated() {
+                for position in 0..<line.count {
+                    if line.characterAt(at: position) == "#" {
+                        stableRocks[position].append(lineIndex)
+                        puzzle[position].append(0)
+                    }
+                    else if line.characterAt(at: position) == "O" {
+                        var lastPuzzlePosition = puzzle[position].count - 1
+                        if lastPuzzlePosition == -1 { puzzle[position].append(0); lastPuzzlePosition += 1 }
+                        puzzle[position][lastPuzzlePosition] += 1
+                    }
+                }
+            }
+            
+            
+            newPuzzleLines = []
+            for position in 0..<lines[0].count {
+                newPuzzleLines.append(getLineUsing(rocks: stableRocks[position], puzzleLine: puzzle[position], len: lines.count))
+            }
+            lines = newPuzzleLines
+            
+            // EAST
+            
+            puzzle      = Array.init(repeating: [0], count: lines[0].count)
+            stableRocks = Array.init(repeating: [], count: lines[0].count)
+            
+            for (lineIndex, line) in lines.enumerated() {
+                for position in 0..<line.count {
+                    if line.characterAt(at: position) == "#" {
+                        stableRocks[position].append(lineIndex)
+                        puzzle[position].append(0)
+                    }
+                    else if line.characterAt(at: position) == "O" {
+                        var lastPuzzlePosition = puzzle[position].count - 1
+                        if lastPuzzlePosition == -1 { puzzle[position].append(0); lastPuzzlePosition += 1 }
+                        puzzle[position][lastPuzzlePosition] += 1
+                    }
+                }
+            }
+            
+            newPuzzleLines = []
+            for position in 0..<lines[0].count {
+                newPuzzleLines.append(getLineUsing(rocks: stableRocks[position], puzzleLine: puzzle[position], len: lines.count))
+            }
+            lines = newPuzzleLines
+            
+            // COMPUTE CYCLE
+            
+            if let _ = cycleChecker[lines] { break }
+            
+            var ans: Int = 0
+            let height = lines.count
+            // Keep stableRocks as north's option
+            for j in 0..<height {
+                ans += (lines[j].components(separatedBy: "O").count - 1) * (height - j)
+            }
+
+            cycleCount.append(ans)   // Save the computation (maybe too expensive ?)
+            cycleChecker[lines] = i  // Save the cycle
         }
         
-        return ans
+        var index: Int = cycleChecker[lines]!
+        index = (1000000000 - cycleChecker.count - 1) % (cycleChecker.count - index) + index
+        return cycleCount[index] // Get the computation at the right cycle
     }
 }
