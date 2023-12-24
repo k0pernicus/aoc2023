e1f861f63144f10985cf9f8ef5dbec795b7374bd
k0pernicus <dev@carette.xyz>
2023-12-24 23:06:53 +0100

    Added result 47 in Day 24 Part 2

--- Tests/DayTests/Tests.swift
+++ Tests/DayTests/Tests.swift
@@ -223,5 +223,5 @@ final class D24Tests: XCTestCase, TestProtocol {
     typealias Output01 = Int
     typealias Output02 = Int
     func testPart01() { part01(filename: "\(day.tag)", expected_result: 2) }
-    func testPart02() { part02(filename: "\(day.tag)", expected_result: 0) }
+    func testPart02() { part02(filename: "\(day.tag)", expected_result: 47) }
 }
